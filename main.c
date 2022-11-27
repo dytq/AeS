666dbe6aa28c3ca2f1fb0b763340e819eb81f9c1
dytq <z5bu8mkb@duck.com>
2022-11-27 14:32:27 +0100

    ajout affichage et round

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #define SIZE_AES 128
 #define TAB_SIZE (SIZE_AES/8)
+#define SIZE_COLUMN 4
 
 #define MULT_3(X) ((X << 1) ^ X)
 #define MULT_2(X) (X << 1)
@@ -67,71 +68,92 @@ int SHIFT_PERMUTATION_TABLE_8_OCT[TAB_SIZE] =
 	0, 1, 2, 3, 5, 6, 7, 4, 10, 11, 8, 9, 15, 12, 13, 14
 };
 
+// Key gen 
+unsigned char RCON[10][4] =
+{
+	{ 0x01, 0, 0, 0 },
+	{ 0x02, 0, 0, 0 },
+	{ 0x04, 0, 0, 0 },
+	{ 0x08, 0, 0, 0 },
+	{ 0x10, 0, 0, 0 },
+	{ 0x20, 0, 0, 0 },
+	{ 0x40, 0, 0, 0 },
+	{ 0x80, 0, 0, 0 },
+	{ 0x1B, 0, 0, 0 },
+	{ 0x36, 0, 0, 0 }
+};
+
 // Signatures
-int get_naif_bit_poid_plus_fort(unsigned short reste);
-unsigned short naif_division_euclidienne(unsigned short dividende, unsigned short diviseur);
-unsigned char * xor_key(unsigned char * s_table, unsigned char * k);
+// 	1. Key Gen
+unsigned char * key_gen(unsigned char * k, unsigned char * k_i, int index_k);
+
+//	2. Transformations function
+unsigned char * sub_byte_column(unsigned char * s_table, int column); 
 unsigned char * sub_byte(unsigned char * s_table);
 unsigned char * shift_row(unsigned char * s_table);
 unsigned char * mix_column(unsigned char * s_table);
+unsigned char * xor_key(unsigned char * s_table, unsigned char * k);
+
+//	3. Other function
+int get_naif_bit_poid_plus_fort(unsigned short reste);
+unsigned short naif_division_euclidienne(unsigned short dividende, unsigned short diviseur);
+void print_table(unsigned char * table, int tab_size, int padding);
 
 // Fonctions
-int get_naif_bit_poid_plus_fort(unsigned short reste) 
+// 	1. Key Gen 
+unsigned char * key_gen(unsigned char * k, unsigned char * k_i, int index_k)
 {
-	int bit_poids_plus_fort = 0;
-	while(reste != 1) 
-	{
-		reste = reste >> 1;
-		bit_poids_plus_fort ++;
-	}
-	return bit_poids_plus_fort;
-}
-
-// return reste
-unsigned short naif_division_euclidienne(unsigned short dividende, unsigned short diviseur)
-{
-	unsigned short reste = dividende;
+	// ROT WORD
+	k_i[COLUMN[0][0]] = k[COLUMN[3][1]];
+	k_i[COLUMN[0][1]] = k[COLUMN[3][2]];
+	k_i[COLUMN[0][2]] = k[COLUMN[3][3]];
+	k_i[COLUMN[0][3]] = k[COLUMN[3][0]];
 	
-	int bit_poids_plus_fort_diviseur = get_naif_bit_poid_plus_fort(diviseur);
-	int bit_poids_plus_fort_reste = get_naif_bit_poid_plus_fort(reste);
+	// SUB Byte
+	k_i = sub_byte_column(k_i, 0);
 
-	while(bit_poids_plus_fort_reste >= bit_poids_plus_fort_diviseur) 
+	// xor column 
+	k_i[COLUMN[0][0]] ^= k[COLUMN[0][0]] ^ RCON[index_k][0];
+	k_i[COLUMN[0][1]] ^= k[COLUMN[0][1]] ^ RCON[index_k][1];
+	k_i[COLUMN[0][2]] ^= k[COLUMN[0][2]] ^ RCON[index_k][2];
+	k_i[COLUMN[0][3]] ^= k[COLUMN[0][3]] ^ RCON[index_k][3];
+		
+	for(int i = 1; i < 4; i++)
 	{
-		reste = reste ^ (diviseur << (bit_poids_plus_fort_reste - bit_poids_plus_fort_diviseur));
-		bit_poids_plus_fort_reste = get_naif_bit_poid_plus_fort(reste);
+		for(int j = 0; j < 4; j++)
+		{
+			k_i[COLUMN[i][j]] = k[COLUMN[i][j]] ^ k_i[COLUMN[i-1][j]];
+		}
 	}
-	if(reste > 0xFF)
-	{
-		fprintf(stderr, "return value overflow"); 
-	}
-	return reste;
+
+	return k_i;
 }
 
-unsigned char * xor_key(unsigned char * s_table, unsigned char * k)
-{
-	for(int i = 0; i < TAB_SIZE; i++)
-	{
-		s_table[i] = s_table[i] ^ k[i];
-	}
-	return s_table;
-}
-
-unsigned char * sub_byte(unsigned char * s_table)
+//	2. Transformations function
+unsigned char * sub_byte_column(unsigned char * s_table, int column) 
 {
 	struct Vector2D {
 		int x;
 		int y;
 	};
-
-	for(int i = 0; i < TAB_SIZE; i++)
+	for(int i = 0; i < SIZE_COLUMN; i++)
 	{
 		struct Vector2D vector;
-		unsigned char s_val = s_table[i]; 
+		unsigned char s_val = s_table[COLUMN[column][i]]; 
 		
 		vector.x = (s_val & 0xF0) >> 4;
 		vector.y = s_val & 0x0F;
-		
-		s_table[i] = SUB_BYTES_8_OCT[vector.x][vector.y];
+		s_table[COLUMN[column][i]] = SUB_BYTES_8_OCT[vector.x][vector.y];
+	}
+
+	return s_table;
+}
+
+unsigned char * sub_byte(unsigned char * s_table)
+{
+	for(int i = 0; i < 4; i++)
+	{
+		s_table = sub_byte_column(s_table, i);
 	}
 	return  s_table;
 }
@@ -190,38 +212,130 @@ unsigned char * mix_column(unsigned char * s_table)
 	return s_table;
 }
 
-int main()
+unsigned char * xor_key(unsigned char * s_table, unsigned char * k)
 {
-	unsigned char * s_table = MESSAGE; 
-	unsigned char * k = K_0;
-
-	// s xor k_0 
-	s_table = xor_key(s_table, k);
-	
-	// SUB_BYTE 
-	s_table = sub_byte(s_table);
-
-	// SHIFT ROW
-	s_table = shift_row(s_table);
-
-	// MIX COLUMN
-	s_table = mix_column(s_table);
-
-	int index_tab = 0;
-	for(int i = 0; i < TAB_SIZE/4; i++)
+	for(int i = 0; i < TAB_SIZE; i++)
 	{
-		for(int j = 0; j < TAB_SIZE/4; j++)
+		s_table[i] = s_table[i] ^ k[i];
+	}
+	return s_table;
+}
+
+//	3. Other function
+int get_naif_bit_poid_plus_fort(unsigned short reste) 
+{
+	int bit_poids_plus_fort = 0;
+	while(reste != 1) 
+	{
+		reste = reste >> 1;
+		bit_poids_plus_fort ++;
+	}
+	return bit_poids_plus_fort;
+}
+
+// return reste
+unsigned short naif_division_euclidienne(unsigned short dividende, unsigned short diviseur)
+{
+	if(dividende == 0)
+	{
+		return 0;
+	}
+	unsigned short reste = dividende;
+	
+	int bit_poids_plus_fort_diviseur = get_naif_bit_poid_plus_fort(diviseur);
+	int bit_poids_plus_fort_reste = get_naif_bit_poid_plus_fort(reste);
+
+	while(bit_poids_plus_fort_reste >= bit_poids_plus_fort_diviseur) 
+	{
+		reste = reste ^ (diviseur << (bit_poids_plus_fort_reste - bit_poids_plus_fort_diviseur));
+		bit_poids_plus_fort_reste = get_naif_bit_poid_plus_fort(reste);
+	}
+	if(reste > 0xFF)
+	{
+		fprintf(stderr, "return value overflow"); 
+	}
+	return reste;
+}
+
+void print_table(unsigned char * table, int tab_size, int padding)
+{
+	int index_tab = 0;
+	for(int i = 0; i < tab_size/padding; i++)
+	{
+		for(int j = 0; j < tab_size/padding; j++)
 		{
-			if(s_table[index_tab] < 0x0F)
+			if(table[index_tab] < 0x0F)
 			{
-				printf("0%X ", s_table[index_tab]);
+				printf("0%X ", table[index_tab]);
 			}
-			else {
-				printf("%X ", s_table[index_tab]);
+			else 
+			{
+				printf("%X ", table[index_tab]);
 			}
 			index_tab++;
 		}
 		printf("\n");
 	}
+	printf("\n");
+}
+
+int main()
+{
+	unsigned char * s_table = MESSAGE; 
+	
+	printf("MESSAGE: \n");
+	print_table(s_table, TAB_SIZE, 4);
+
+	unsigned char k_table[11][TAB_SIZE] = {{0},{0}};
+	unsigned char * k_ptr_i[11];
+
+	for(int i = 0; i < 11; i++)
+	{
+		k_ptr_i[i] = k_table[i];
+	}
+	
+	k_ptr_i[0] = K_0;
+	k_ptr_i[1] = key_gen(K_0, k_ptr_i[1], 0);
+	
+	for(int i = 2; i < 11; i++)
+	{
+		k_ptr_i[i] = key_gen(k_ptr_i[i-1], k_ptr_i[i], i-1);
+	}
+
+	printf("KEYS K:\n");
+	for(int i = 0; i < 11; i++)
+	{
+		printf("K_%d\n",i);
+		print_table(k_ptr_i[i], TAB_SIZE, 4);
+	}
+
+
+	// s xor k_0 
+	s_table = xor_key(s_table, k_ptr_i[0]);
+	
+	for(int k = 1; k < 11; k++)
+	{
+		// PRINT KEY 
+		printf("ROUND %d\n", k-1);
+		print_table(s_table, TAB_SIZE, 4);
+		
+		// SUB_BYTE 
+		s_table = sub_byte(s_table);
+
+		// SHIFT ROW
+		s_table = shift_row(s_table);
+
+		// MIX COLUMN
+		if((k-1) < 9) 
+		{
+			s_table = mix_column(s_table);
+		}
+		
+		// XOR KEY 
+		s_table = xor_key(s_table, k_ptr_i[k]);
+	}
+
+	printf("RESULTAT: \n");
+	print_table(s_table, TAB_SIZE,4);
 	return 0;
 }
